6c641b74e6bb2a732060134e038aaf28f026184f
Matthew Lonis <mattlonis@gmail.com>
2023-11-20 23:12:34 -0500

    Cleaning up code

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ poetry run pylint src tests scrape_web.py
 poetry run pydocstyle src tests scrape_web.py
 poetry run bandit -c pyproject.toml -r .
 poetry run pytest --cov --cov-report=html -n auto
+# poetry run python scrape_web.py
